8fb1f2db92aa1cf0fa1acd4175f5af5c1756f510
oluwajohnson <ogunleyejohnson77@gmail.com>
2023-10-30 16:28:33 +0100

    Basic Programming

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+
+        // What is your first name? Scott
+        // What is your last name? Burton
+        // Your name is Burton, Scott Burton.
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        string myfirstName = char.ToUpper(firstName.First()) + firstName.Substring(1).ToLower();
+        string mylastName = char.ToUpper(lastName.First()) + lastName.Substring(1).ToLower();
+
+        Console.Write($"Your name is {mylastName}, {myfirstName} {mylastName}.");
+
     }
 }
