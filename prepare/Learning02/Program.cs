a9b67ff0e10a1fb5291a697663580cc3a9899e21
oluwajohnson <ogunleyejohnson77@gmail.com>
2023-11-09 16:25:46 +0100

    Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+    // Job job1 = new Job();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Microsoft",
+            _startYear = 2019,
+            _endYear = 2022
+        };
+
+        Job job2 = new Job
+        {
+            _jobTitle = "Manager",
+            _company = "Apple",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        Resume myResume = new Resume
+        {
+            _name = "Allison Rose"
+        };
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
