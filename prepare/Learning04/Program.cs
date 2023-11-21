6fc1e10b7f4d3611fe42d440041d407e836fb8aa
oluwajohnson <ogunleyejohnson77@gmail.com>
2023-11-21 21:43:06 +0100

    Learning 03

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assign = new Assignment("Ogunleye Johnson ", "C# Project");
+        Console.WriteLine(assign.GetSummary());
+
+
+        MathAssignment mathAssign = new MathAssignment("Gbenga Lanre", "Phyton", "6", "Classes");
+
+        Console.WriteLine(mathAssign.GetSummary());
+
+        Console.WriteLine(mathAssign.GetHomeworkList());
+
+        WritingAssignment writtingAssign = new WritingAssignment("Gbemi  Daniel", "Research Project", "Mobile application");
+
+        Console.WriteLine(writtingAssign.GetSummary());
+
+        Console.WriteLine(writtingAssign.GetWritingInformation());
     }
 }
