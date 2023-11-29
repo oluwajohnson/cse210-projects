7a0ce836e8152ebe0660a85dfb0266ed38a8a2af
oluwajohnson <ogunleyejohnson77@gmail.com>
2023-11-29 20:51:46 +0100

    learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 5);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow", 8, 2);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Red", 9);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+
+            string color = s.GetColor();
+
+
+            double area = s.GetArea();
+
+            Console.WriteLine(color + " shape get area " + area);
+        }
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -69,6 +69,9 @@ class Program
     }
 
 
+
+
+
     static void Main(string[] args)
     {
 
